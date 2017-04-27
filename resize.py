7611f8ea26a6dfde7304f8bd4ecc4fcebb00b1c7
Pablo Blanco <pablinho.blanco@gmail.com>
2017-04-27 15:38:51 +0200

    Create resize.py

---
+++ resize.py
@@ -0,0 +1,5 @@
+config = {'st3v3' : mkDict('password',   'blonde',   'Steve Booker'),
+          'blubb' : mkDict('12345678',   'black',    'Bubb Ohaal'),
+          'suprM' : mkDict('kryptonite', 'black',    'Clark Kent'),
+          #...
+         }
