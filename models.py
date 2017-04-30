e277effad8a4b47afd3d7c000b38fecf85f71289
pablinho <pablinho.blanco@gmail.com>
2017-04-30 12:35:55 +0200

    Resizer, first test

---
+++ models.py
@@ -0,0 +1,26 @@
+##RESIZES
+
+#6'0" = 1.83
+#6'2" = 1.88
+#6'4" = 1.93
+#6'6" = 1.98
+#6'8" = 2.03
+
+originMainPath = 'surfboards'
+destinationMainPath = 'finalSurfboardSizes'
+config = {
+  'stuby' : {
+    'origin': [
+        'stuby/gcode_stuby64/0-bottom_FINAL.gcode',
+        'stuby/gcode_stuby64/1-top_FINAL.gcode'
+    ],
+    'destination': [
+        '0-bottom_FINAL.gcode',
+        '1-top_FINAL.gcode'
+    ],
+    'resizes': {
+      '6and4': [ 1, 1, 1 ],
+      '6and6': [ 1.0259, 1, 1 ]
+    }
+  }
+}
